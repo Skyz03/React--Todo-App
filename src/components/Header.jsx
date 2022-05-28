02b06b0361599dab82cd9f7c93dcc7822a4b6ca4
Skyz03 <aakibdiscoo@gmail.com>
2022-05-28 19:59:01 +0545

    Updates in the Header just starting

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="container mx-auto px-4 ">Hello world!</h1>
+    </div>
+  );
+};
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    theme: {
+      container: {
+        center: true,
+      },
+    },
   },
   plugins: [],
 };
