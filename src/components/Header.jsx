6b3192bd83496c35ce367c3e4f0edf4067a983e0
Skyz03 <aakibdiscoo@gmail.com>
2022-05-28 20:41:49 +0545

    Updates in the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import icon from "../img/icon-moon.svg";
 
 const Header = () => {
   return (
-    <div>
-      <h1 className="container mx-auto px-4 ">Hello world!</h1>
+    <div className="bg-hero-pattern min-h-screen bg-no-repeat">
+      <main className="mx-auto w-[87%] max-w-[34rem] py-10 md:py-20">
+        {/* Top Title with button  */}
+        <div className="flex items-center justify-between">
+          <h1 className="text-2xl font-semibold uppercase text-white tracking-[.8rem]">
+            Todo
+          </h1>
+
+          <div>
+            <button className="cursor-pointer" type="button">
+              <img src={icon} alt="theme-icon" />
+            </button>
+          </div>
+        </div>
+
+        {/* Input Component  */}
+      </main>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('/src/img/bg-mobile-dark.jpg')",
+      },
+    },
     theme: {
       container: {
         center: true,
