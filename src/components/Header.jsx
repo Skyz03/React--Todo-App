3fb1bc09f8d870afe3ff6e405e454bc2be2053f4
Skyz03 <aakibdiscoo@gmail.com>
2022-05-29 16:08:29 +0545

    Updates in the mobile design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import InputList from "./InputList";
 
 const Header = () => {
   return (
-    <div className="bg-hero-pattern min-h-screen bg-no-repeat">
+    <div className="bg-hero-pattern min-h-screen bg-no-repeat bg-slate-300">
       <main className="mx-auto w-[87%] max-w-[34rem] py-10 md:py-20">
         {/* Top Title with button  */}
         <div className="flex items-center justify-between">
--- src/components/InputList.jsx
+++ src/components/InputList.jsx
@@ -3,7 +3,7 @@ import cross from "../img/icon-cross.svg";
 
 const InputAdd = () => {
   return (
-    <div className="mt-4 shadow-2xl">
+    <div className="mt-4 shadow-2xl shadow-black/20 md:mt-6">
       <ul className="w-full">
         <li className="flex w-100 bg-white items-center py-3 px-4">
           <button className="rounded-full border w-5 aspect-square"></button>
@@ -15,6 +15,33 @@ const InputAdd = () => {
           </div>
         </li>
       </ul>
+      <div className="w-100 border-t flex items-center justify-between  rounded-md px-4 py-3 bg-white space-x-4">
+        <span className="text-xs text-xs text-light-text-primary dark:text-dark-text-quaternary md:text-sm">
+          1 Item left
+        </span>
+
+        {/* <div className="flex justify-between">
+          <button className="text-xs font-bold text-light-text-primary">
+            All
+          </button>
+          <button className="text-xs font-bold  px-1 text-light-text-primary">
+            Active
+          </button>
+          <button className="text-xs font-bold text-light-text-primary">
+            Completed
+          </button>
+        </div> */}
+
+        <button className="text-xs text-light-text-primary">
+          Clear Completed
+        </button>
+      </div>
+
+      <div className="mt-4 bg-white px-4 py-3 flex items-center justify-center space-x-4 rounded">
+        <button className="text-xs text-light-text-primary">All</button>
+        <button className="text-xs text-light-text-primary">Active</button>
+        <button className="text-xs text-light-text-primary">Completed</button>
+      </div>
     </div>
   );
 };
